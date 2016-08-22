26f61d1ab0334521c56cbe8b8ba5e5db04208cb9
Clint <cngodles@gmail.com>
2016-08-22 16:53:21 -0400

    Accuracy Change
    Allow easy change to accuracy via console push

--- js/planets.main.js
+++ js/planets.main.js
@@ -2,19 +2,20 @@ var planet = {
 	loaded:false,
   	focalsize:1,
   	focusvalue:1,
+	accuracy:2,
 	targetbody:'Sol',
   	data:[
-		{name:'Sol', dia:1392000},
-		{name:'Mercury', dia:4879},
-		{name:'Venus', dia:12104},
-		{name:'Earth', dia:12756},
-		{name:'Moon', dia:3475},
-		{name:'Mars', dia:6792},
-		{name:'Jupiter', dia:142984},
-		{name:'Saturn', dia:120536},
-		{name:'Uranus', dia:51118},
-		{name:'Neptune', dia:49528},
-		{name:'Pluto', dia:2370}
+		{name:'Sol', v:1392000},
+		{name:'Mercury', v:4879},
+		{name:'Venus', v:12104},
+		{name:'Earth', v:12756},
+		{name:'Moon', v:3475},
+		{name:'Mars', v:6792},
+		{name:'Jupiter', v:142984},
+		{name:'Saturn', v:120536},
+		{name:'Uranus', v:51118},
+		{name:'Neptune', v:49528},
+		{name:'Pluto', v:2370}
   	],
   	setMeasurements:function(){
 		var i = 0;
@@ -29,7 +30,7 @@ var planet = {
 		}
 		//Create HTML for output.
 		for(i=0; this.data.length > i; i++){
-			var finaldia = (this.focalsize * (this.data[i].dia / this.focusvalue)).toFixed(2);
+			var finaldia = (this.focalsize * (this.data[i].v / this.focusvalue)).toFixed(this.accuracy);
 			returndata.push('<li class="planetdata" rel="'+this.data[i].name+'">'+this.data[i].name+': <span>'+finaldia+'</span> cm</li>');
 		}
 		$("#return_dia").html(returndata.join("\n"));
